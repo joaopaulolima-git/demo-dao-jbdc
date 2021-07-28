ca5e0cbfbc6ed777882d4d2ae80e8beab39cf481
João Paulo <jplopapa.git@gmail.com>
2021-07-28 10:07:12 -0300

    findByDepartment implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public class DaoFactory {
+import db.DB;
+import model.dao.impl.SellerDaoJBDC;
 
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJBDC(DB.getConnection());
+	}
 }
+
+
